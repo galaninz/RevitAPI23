ab5e011f02fee6e6b88bfcf7b123ee801c151615
Zakhar Galanin <zakhargalanin@gmail.com>
2022-09-03 01:19:24 -0700

    Add project files.

---
+++ RevitAPI23/Main.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI23
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> familyInstancesS = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_StructuralColumns)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            List<FamilyInstance> familyInstancesA = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Columns)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            
+
+
+            TaskDialog.Show("Columns count", $"Несущих колонн: {familyInstancesS.Count.ToString()}, Архитектурных: { familyInstancesA.Count.ToString()}");
+
+            return Result.Succeeded;
+        }
+    }
+}
